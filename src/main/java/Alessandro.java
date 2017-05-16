f21385825a2bc2ee0d17b1f97bf560fd8ab153a0
Xander_C <alessandro.cevoli2@studio.unibo.it>
2017-05-16 15:28:39 +0200

    added basic Java build, gradle build and travis configuration

---
+++ src/main/java/Alessandro.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class Alessandro{
+
+	public Alessandro(){
+		System.out.println("Alessandro");
+	}
+
+	public static void main(final String[] args){
+
+		new Alessandro();
+	}
+
+}
